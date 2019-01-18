18ca0443b515e8af65fe9e5f4f0cd17b4e63a600
Zeki Gulser <zeki@backbase.com>
2019-01-18 08:47:58 +0100

    commit wohooo

--- app/src/main/java/com/android/buzzaway/securecards/ConfirmationActivity.java
+++ app/src/main/java/com/android/buzzaway/securecards/ConfirmationActivity.java
@@ -1,6 +1,7 @@
 package com.android.buzzaway.securecards;
 
 import android.animation.Animator;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.VectorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 public class ConfirmationActivity extends AppCompatActivity {
 
     private ImageView animatedLogo;
+    private ImageView logo;
     private TextView status;
     private static int animationCount = 0;
 
@@ -23,10 +25,11 @@ public class ConfirmationActivity extends AppCompatActivity {
 
         status = findViewById(R.id.status);
         animatedLogo = findViewById(R.id.logo);
+        logo = findViewById(R.id.logo2);
         VectorDrawable progress = (VectorDrawable) getResources().getDrawable(R.drawable.time);
         animatedLogo.setImageDrawable(progress);
         ViewPropertyAnimator animator = animatedLogo.animate();
-        animator.rotation(360.0f).setDuration(2000).setListener(new Animator.AnimatorListener() {
+        animator.rotation(360.0f).setDuration(1500).setListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) { }
 
@@ -34,7 +37,7 @@ public class ConfirmationActivity extends AppCompatActivity {
             public void onAnimationEnd(Animator animator) {
                 if (animationCount < 2) {
                     float rotation = animatedLogo.getRotation();
-                    animatedLogo.animate().rotation(rotation == 360.0f ? 0.0f : 360.0f).setDuration(2000).start();
+                    animatedLogo.animate().rotation(rotation == 360.0f ? 0.0f : 360.0f).setDuration(1500).start();
                 } else {
                     AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.finaldone);
                     animatedLogo.setImageDrawable(animatedVectorDrawable);
@@ -43,6 +46,7 @@ public class ConfirmationActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             status.setVisibility(View.VISIBLE);
+                            logo.setImageBitmap(takescreenshot(getWindow().getDecorView().getRootView()));
                         }
                     }, 1500);
                 }
@@ -56,5 +60,13 @@ public class ConfirmationActivity extends AppCompatActivity {
             public void onAnimationRepeat(Animator animator) { }
         });
         animator.start();
+    }
+
+    public static Bitmap takescreenshot(View v) {
+        v.setDrawingCacheEnabled(true);
+        v.buildDrawingCache(true);
+        Bitmap b = Bitmap.createScaledBitmap(v.getDrawingCache(), 200, 430, true);
+        v.setDrawingCacheEnabled(false);
+        return b;
     }
 }
